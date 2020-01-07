a5dc878e9b5efecc0e6d478e8c10749e7dc2fe89
p.bailey <pabailey123@gmail.com>
2020-01-07 16:04:55 -0500

    added code pieces and code editor panels to game screen

--- src/main/java/GameMechanicPrototype/GameScreen.java
+++ src/main/java/GameMechanicPrototype/GameScreen.java
@@ -5,10 +5,37 @@ import java.awt.*;
 
 public class GameScreen extends JFrame {
 
+    public static void main(String[] args){
+        GameScreen screen = new GameScreen(new Dimension(1500, 1000));
+        screen.setVisible(true);
+    }
+
     public GameScreen(Dimension dimension) {
         this.setSize(dimension);
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        this.setName("Main Game Screen");
         this.setTitle("Learn TDD");
+        Container contentPane = this.getContentPane();
+        contentPane.setLayout(new FlowLayout());
+        contentPane.setName("Main Screen Content Pane");
+
+        JPanel codePiecesPanel = new JPanel();
+        codePiecesPanel.setName("Code Pieces");
+        contentPane.add(codePiecesPanel);
+
+        JLabel codePiecesLabel = new JLabel();
+        codePiecesLabel.setText("CODE PIECES");
+        codePiecesPanel.add(codePiecesLabel);
+
+        JPanel codeEditorPanel = new JPanel();
+        codeEditorPanel.setName("Code Pieces");
+        contentPane.add(codeEditorPanel);
+
+        JLabel codeEditorLabel = new JLabel();
+        codeEditorLabel.setText("CODE EDITOR");
+        codeEditorPanel.add(codeEditorLabel);
     }
+
+
 
 }
--- src/test/java/GameMechanicPrototype/GameScreenTest.java
+++ src/test/java/GameMechanicPrototype/GameScreenTest.java
@@ -2,6 +2,7 @@ package GameMechanicPrototype;
 
 import org.junit.Test;
 
+import javax.swing.*;
 import java.awt.*;
 
 import static org.junit.Assert.*;
@@ -25,6 +26,14 @@ public class GameScreenTest {
     }
 
     @Test
+    public void gameScreenShouldHaveNameOfMainGameScreen(){
+        GameScreen screen = new GameScreen(new Dimension(2, 2));
+        String expectedName = "Main Game Screen";
+
+        assertEquals(expectedName, screen.getName());
+    }
+
+    @Test
     public void gameScreenTitleDefaultsToLearnTDD() {
         GameScreen screen = new GameScreen(new Dimension(1500,1000));
         String expectedTitle = "Learn TDD";
@@ -32,5 +41,47 @@ public class GameScreenTest {
         assertEquals(expectedTitle, screen.getTitle());
     }
 
+    @Test
+    public void gameScreenContentPaneShouldHaveFlowLayout(){
+        GameScreen screen = new GameScreen(new Dimension(1, 1));
+        String expectedLayout = FlowLayout.class.toString();
 
+        assertEquals(expectedLayout, screen.getContentPane().getLayout().getClass().toString());
+    }
+
+    @Test
+    public void gameScreenContentPaneShouldHaveNameOfMainScreenContentPane(){
+        GameScreen screen = new GameScreen(new Dimension(1, 1));
+        String expectedName = "Main Screen Content Pane";
+
+        assertEquals(expectedName, screen.getContentPane().getName());
+    }
+
+    @Test
+    public void firstJPanelisNamedCodePieces() {
+        GameScreen screen = new GameScreen(new Dimension(1, 1));
+        String expectedName = "Code Pieces";
+
+        assertEquals(expectedName, screen.getContentPane().getComponent(0).getName());
+    }
+
+    @Test
+    public void codePiecesPanelShouldHaveLabelWithTextThatSaysCodePieces(){
+        GameScreen screen = new GameScreen(new Dimension(2,2));
+        String expectedText = "CODE PIECES";
+        JPanel panel = (JPanel) screen.getContentPane().getComponent(0);
+        JLabel label = (JLabel) panel.getComponent(0);
+
+        assertEquals(expectedText, label.getText());
+    }
+
+    @Test
+    public void codeEditorPanelShouldHaveLabelWithTextThatSaysCodeEditor() {
+        GameScreen screen = new GameScreen(new Dimension(2,2));
+        String expectedText = "CODE EDITOR";
+        JPanel panel = (JPanel) screen.getContentPane().getComponent(1);
+        JLabel label = (JLabel) panel.getComponent(0);
+
+        assertEquals(expectedText, label.getText());
+    }
 }
